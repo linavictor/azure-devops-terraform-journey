8402b5578935247332f9fa59696f10d7b44ab5cd
Miriam Lina <linaeie22@gmail.com>
2026-04-29 16:16:58 +0000

    Fix pipeline location

---
+++ environments/dev/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
